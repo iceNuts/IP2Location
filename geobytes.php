2c4db5dea776c7b28c1eb78d68f2e72e7b1e8e43
Li Zeng <BillZeng@Lis-MacBook-Air.local>
2014-09-16 14:58:02 -0500

    Test geobytes;

---
+++ geobytes.php
@@ -0,0 +1,10 @@
+<?php
+    
+    $tags = get_meta_tags('http://www.geobytes.com/IpLocator.htm?GetLocation&template=php3.txt&IpAddress=216.87.60.29');
+    print $tags['city'];  // city name
+
+    /* my current IP is in Kansas City in Tunespeak Office
+     * Free yep.. but it seems not accurate
+     * easy to use, one line of code
+     */
+
